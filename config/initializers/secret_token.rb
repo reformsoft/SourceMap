0b536ad20f77667090d684e40b4a8663209969ca
Kieran Senior <kieran@kieransenior.co.uk>
2012-05-14 20:50:48 +0100

    Add the rest of the files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapTap::Application.config.secret_token = 'd9cad54591c960152ba83e51ea5c4f36efd2c6ffd28524877123479c1e0cab5c55cba15c66c4dfd8bdd4ed82c394ad67d0c69793584596178cf9fc9c0fbf95be'
