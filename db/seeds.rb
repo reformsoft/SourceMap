83d5b076001f698716d2624aebb624e6d9b94e73
deedoubleyou <deedoubleyou@gmail.com>
2013-01-17 20:47:32 +0000

    Added a couple of real life Guernsey locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,33 +15,27 @@ Tag.destroy_all
 Service.destroy_all
 Location.destroy_all
 
-Location.create(
-	:lat => 1,
-	:lng => 1
-	)
+#####
+# China Red
+#####
+Location.create(:lat => 49.46392, :lng => -2.53518)
+Service.create( { :name => "China Red", :location => Location.last }, :without_protection => true)
+Tag.create( { :name => "Restaurant", :services => [ Service.last ] }, :without_protection => true)
+Tag.create( { :name => "Chinese", :services => [ Service.last ] }, :without_protection => true)
+Tag.create( { :name => "Takeaway", :services => [ Service.last ] }, :without_protection => true)
 
-	Service.create(
-		[
-		:name => "Supermarket with a cashpoint",
-		:location => Location.last
-		], 
-		:without_protection => true
-		)
-
-Tag.create(
-	[
-	:name => "Cashpoint",
-	:services => [ Service.last ]
-	],
-	:without_protection => true
-	)
-
-Tag.create(
-	[
-	:name => "Supermarket",
-	:services => [ Service.last ]
-	],
-	:without_protection => true
-	)
+#####
+# Guernsey Airport
+#####
+Location.create(:lat => 49.43197, :lng => -2.59643)
+Service.create({ :name => "Guernsey Airport", :location => Location.last }, :without_protection => true)
+Tag.create({:name => "Airport", :services => [ Service.last ] }, :without_protection => true)
 
 
+#####
+# Captin's
+#####
+Location.create(:lat => 49.43177, :lng => -2.55713)
+Service.create( { :name => "Captain's", :location => Location.last }, :without_protection => true)
+Tag.find_by_name("Restaurant").services << Service.last
+Tag.create({:name => "Pub", :services => [ Service.last ] }, :without_protection => true)
